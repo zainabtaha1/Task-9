95e01da80afbe2e34bea339bda5df01b62036c13
Zainab El Sayed <121588138+zainabtaha1@users.noreply.github.com>
2023-02-28 04:23:47 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Problem 5 section 3
+            const float PI = 3.14f;
+            Console.WriteLine(" Enter the raduis ");
+            double raduis = double.Parse(Console.ReadLine());
+            double area = PI * raduis * raduis;
+            Console.WriteLine(" The area equals : " + area);
+        }
+    }
+}
